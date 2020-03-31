e93eaf898ac7295e994fde2a8bc39f9bcf93856d
ultraluxe <ultraluxe43@gmail.com>
2020-04-01 03:32:16 +0500

    v0.1
    First major styling update

---
+++ scripts/scrollreveal.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.wp-club');
+ScrollReveal().reveal('td');
+ScrollReveal().reveal('.bott');
+ScrollReveal().reveal('.name');
+ScrollReveal().reveal('.place');
+ScrollReveal().reveal('.num');
