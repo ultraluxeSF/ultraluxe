6f615490265147294da2ad808a6c0b0e766bb5e8
ultraluxe <ultraluxe43@gmail.com>
2020-06-06 16:51:15 +0500

    0.2.4 404 page + current year.js

---
+++ scripts/year.js
@@ -0,0 +1,4 @@
+let currentYear = new Date().getFullYear();
+document.querySelector("#year").textContent = currentYear;
+
+// This script shows the current year at the copyright section on the footer
