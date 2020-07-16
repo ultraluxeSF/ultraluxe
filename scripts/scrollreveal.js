a3321968cb53d105d66546fd51b96460d2440e19
ultraluxe <ultraluxe43@gmail.com>
2020-07-16 18:20:29 +0500

    0.3.1 home.html +ScrollReveal

--- scripts/scrollreveal.js
+++ scripts/scrollreveal.js
@@ -1,6 +1,17 @@
+// portfolio.html
 ScrollReveal().reveal('.wp-club');
 ScrollReveal().reveal('td');
 ScrollReveal().reveal('.bott');
 ScrollReveal().reveal('.name');
 ScrollReveal().reveal('.place');
 ScrollReveal().reveal('.num');
+
+
+// home.html
+ScrollReveal().reveal('blockquote');
+ScrollReveal().reveal('.h1');
+ScrollReveal().reveal('p');
+ScrollReveal().reveal('figure');
+ScrollReveal().reveal('.viewportfolio');
+ScrollReveal().reveal('.service-ico');
+ScrollReveal().reveal('h2');
