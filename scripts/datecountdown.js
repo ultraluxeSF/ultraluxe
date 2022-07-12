acbe358cacd5718bf764fd0666bba0ae4130887d
ultraluxe <ultraluxe43@gmail.com>
2022-07-12 22:13:48 +0500

    v3.2.7 +mebanner,cvWIP,-2servicePTS

---
+++ scripts/datecountdown.js
@@ -0,0 +1,21 @@
+// Graphic Design
+
+let thisYear = new Date().getFullYear();
+let gdYear = '2017';
+let gdDate = new Date("07-01-2017");
+let currentDate = new Date();
+
+var gdDifference = currentDate - gdDate;
+
+document.querySelector("#xp-GD-difference").textContent = Math.trunc(gdDifference/(1000 * 3600 * 24));
+document.querySelector("#xp-GD-years").textContent = thisYear - gdYear;
+
+// UI/UX
+
+let uiYear = '2021';
+let uiDate = new Date("06-28-2021");
+
+var uiDifference = currentDate - uiDate;
+
+document.querySelector("#xp-UI-difference").textContent = Math.trunc(uiDifference/(1000 * 3600 * 24));
+document.querySelector("#xp-UI-years").textContent = thisYear - uiYear;
