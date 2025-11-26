cf1cc21b41fa84916d42cb29d3f531f12586808a
ultraluxesf <ultraluxe43@gmail.com>
2025-11-27 01:24:59 +0500

    v3.2.2 mesection, main-banner*

---
+++ scripts/float-box.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('.containerr');
+const box = document.querySelector('.float-box');
+
+
+container.addEventListener('mousemove', (e) => {
+const rect = container.getBoundingClientRect();
+const mouseX = e.clientX - rect.left;
+const mouseY = e.clientY - rect.top;
+
+
+const xNorm = (mouseX / rect.width) * 2 - 1;
+const yNorm = (mouseY / rect.height) * 2 - 1;
+
+
+const moveStrength = 50;
+const xMove = -xNorm * moveStrength;
+const yMove = -yNorm * moveStrength;
+
+
+box.style.transform = `translate(${xMove}px, ${yMove}px)`;
+});
+
+
+container.addEventListener('mouseleave', () => {
+box.style.transform = `translate(0, 0)`;
+});
